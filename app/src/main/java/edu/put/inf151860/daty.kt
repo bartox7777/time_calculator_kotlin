8ccfe032cf1924ae7e94a34fb7904595adc781f8
bartox7777 <bartosz.zelek@protonmail.com>
2023-04-25 00:51:52 +0200

    calculate HMS

---
+++ app/src/main/java/edu/put/inf151860/daty.kt
@@ -0,0 +1,11 @@
+package edu.put.inf151860
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class daty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daty)
+    }
+}
